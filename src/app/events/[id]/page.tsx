57c6d4c1431a6ded082ab27cf9588fc18f2f38d1
krishna jain <krishnajain5050@gmail.com>
2024-03-14 01:50:29 +0530

    ui added for idividual space

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Event from '@/ui/AvengersInfo/id/Event'
+import { Navbar } from '@/ui/ComponentExporters'
 
 const Page = ({ params }: { params: { id: number } }) => {
   return (
-    <div>Page</div>
+    <section className='w-screen h-screen bg-black overflow-x-hidden'>
+      <Navbar />
+      <div className='mt-20 p-40'>
+        <Event id={params.id} />
+      </div>
+    </section>
   )
 }
 
--- src/app/series/[id]/page.tsx
+++ src/app/series/[id]/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Series from '@/ui/AvengersInfo/id/Series'
+import { Navbar } from '@/ui/ComponentExporters'
 
 const Page = ({ params }: { params: { id: number } }) => {
   return (
-    <div>
-      <Series id={params.id} />
-    </div>
+    <section className='w-screen h-screen bg-black overflow-x-hidden'>
+      <Navbar />
+      <div className='mt-20 p-40'>
+        <Series id={params.id} />
+      </div>
+    </section>
   )
 }
 
