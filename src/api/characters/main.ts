aa7d63854901c5fa482d3a85cba155bccf6aba4a
krishna jain <krishnajain5050@gmail.com>
2024-03-13 16:22:20 +0530

    character api

--- src/api/characters/main.ts
+++ src/api/characters/main.ts
@@ -1,5 +1,29 @@
-import {  getApiDetails, privateKey, publickey } from "@/utils/utils";
-import axios from "axios";
-export const getCharacters = async () => {
-    const { timestamps, hash } = getApiDetails();
+import {  getApiDetails } from "@/utils/utils";
+import axios from "axios"
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+
+export const getCharacters = async (limit : number = 10) => {
+    const { timestamps, hash , publickey  } = getApiDetails();
+    const url = `${BASE_URL}/characters?ts=${timestamps}&limit=${limit}&apikey=${publickey}&hash=${hash}`
+    const { data } = await axios.get(url);
+    console.log(data.data.results);
+    return data.data.results;
+}
+
+export const getCharacterByName = async(name : string , limit : number = 10) => {
+    const { timestamps, hash , publickey } = getApiDetails();
+    const url = `${BASE_URL}/characters?nameStartsWith=${name}&limit=${limit}&ts=${timestamps}&apikey=${publickey}&hash=${hash}`;
+    const response = await axios.get(url);
+    const { data } = response;
+    console.log(data.data.results);
+    return data.data.results;
+}
+
+export const getCharacterById = async(id : number , limit : number = 10) => {
+    const { timestamps, hash , publickey } = getApiDetails();
+    const url = `${BASE_URL}/characters/${id}?ts=${timestamps}&limit=${limit}&apikey=${publickey}&hash=${hash}`
+    const response = await axios.get(url);
+    const { data } = response;
+    console.log(data.data.results);
 }
--- src/api/characters/types.ts
+++ src/api/characters/types.ts
@@ -1,3 +1,66 @@
-export interface Characters {
-     
-}
+import z from "zod";
+
+const CharacterSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  modified: z.date(),
+  resourceURI: z.string(),
+  urls: z.array(
+    z.object({
+      type: z.string(),
+      url: z.string(),
+    })
+  ),
+  thumbnail: z.object({
+    path: z.string(),
+    extension: z.string(),
+  }),
+  comics: z.object({
+    available: z.string(),
+    returned: z.string(),
+    collectionURI: z.string(),
+    items: z.array(
+      z.object({
+        resourceURI: z.string(),
+        name: z.string(),
+      })
+    ),
+  }),
+  stories: z.object({
+    available: z.string(),
+    returned: z.string(),
+    collectionURI: z.string(),
+    items: z.array(
+      z.object({
+        resourceURI: z.string(),
+        name: z.string(),
+        type: z.string(),
+      })
+    ),
+  }),
+  events: z.object({
+    available: z.string(),
+    returned: z.string(),
+    collectionURI: z.string(),
+    items: z.array(
+      z.object({
+        resourceURI: z.string(),
+        name: z.string(),
+      })
+    ),
+  }),
+  series: z.object({
+    available: z.string(),
+    returned: z.string(),
+    collectionURI: z.string(),
+    items: z.array(
+      z.object({
+        resourceURI: z.string(),
+        name: z.string(),
+      })
+    ),
+  }),
+});
+export const CharactersSchema = z.array(CharacterSchema);
+export type Character = z.infer<typeof CharacterSchema>;
