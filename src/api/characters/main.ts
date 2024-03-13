9009ec00e485c691fddec74bf5626a4b50eb9b80
nischay-chauhan <masternischay@outlook.com>
2024-03-13 13:45:39 +0530

    Hasher function

--- src/api/characters/main.ts
+++ src/api/characters/main.ts
@@ -1,3 +1,5 @@
+import {  getApiDetails, privateKey, publickey } from "@/utils/utils";
+import axios from "axios";
 export const getCharacters = async () => {
-    
+    const { timestamps, hash } = getApiDetails();
 }
---
+++ src/utils/utils.ts
@@ -0,0 +1,12 @@
+import { Md5 } from "ts-md5";
+
+export const publickey = process.env.NEXT_PUBLIC_MARVEL_PUBLIC_KEY!;
+export const privateKey = process.env.NEXT_PUBLIC_MARVEL_PRIVATE_KEY!;
+
+export const getApiDetails = () => {
+
+ 
+    const timestamps = new Date().getTime();
+    const hash = Md5.hashStr(timestamps + privateKey + publickey);
+    return { publickey , privateKey, timestamps, hash };
+  };
