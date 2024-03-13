07f19b13c1613e61d627922bd407237cec0e0b2e
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-03-14 00:10:17 +0530

    configured firebase

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -1,10 +1,11 @@
-"use client"
-import React from 'react'
-import { store } from '@/lib/store'
-import { Provider } from 'react-redux'
+"use client";
+
+import React from "react";
+import { store } from "@/lib/store";
+import { Provider } from "react-redux";
 
 const storeProvider = ({ children }: { children: React.ReactNode }) => {
-    return <Provider store={store}>{children}</Provider>
-}
+  return <Provider store={store}>{children}</Provider>;
+};
 
-export default storeProvider
+export default storeProvider;
