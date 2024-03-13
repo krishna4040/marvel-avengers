ce6c3619818546f1d88643d551550b00adbe0922
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-03-13 21:26:31 +0530

    created the model for club and talks

---
+++ src/lib/DB/models/talks.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema, Model, Document, Types } from "mongoose";
+
+interface Talk extends Document {
+  clubId: Types.ObjectId;
+  talks: {
+    userId: Types.ObjectId;
+    talk: string;
+    time: Date;
+    src: string;
+  }[];
+}
+
+const talkSchema = new Schema<Talk>(
+  {
+    clubId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      unique: true,
+    },
+    talks: {
+      type: [
+        {
+          userId: { type: Types.ObjectId, ref: "user" },
+          talk: { type: String },
+          time: { type: Date, default: new Date() },
+          src: { type: String, default: "" },
+        },
+      ],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const TalkModel: Model<Talk> =
+  mongoose.models["talk"] || mongoose.model<Talk>("talk", talkSchema);
+
+export { TalkModel };
