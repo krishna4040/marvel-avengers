f662c52e5fcfa5200cf8e0fb25afe1ac94e1551c
nischay-chauhan <masternischay@outlook.com>
2024-03-14 11:36:39 +0530

    NEXT aut from /next

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import { Account, User as AuthUser } from "next-auth";
+import NextAuth from "next-auth/next";
+import { Account, User as AuthUser, NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
